fe3f857b47d3917bc1ce9019e9bac75411994b7b
Milos Vasic <milos85vasic@gmail.com>
2021-04-15 18:06:45 +0200

    Cleaning up.

--- Native/Test/test.cpp
+++ Native/Test/test.cpp
@@ -1,11 +1,9 @@
 #include <string>
-#include <iostream>
 #include "library.h"
 #include <gtest/gtest.h>
 
 TEST(BoostLibraryTest, AvailabilityTest) {
 
     auto version = getBoostVersion();
-    std::cout << "Boost version: " << version << std::endl;
     ASSERT_FALSE(version.empty());
 }
