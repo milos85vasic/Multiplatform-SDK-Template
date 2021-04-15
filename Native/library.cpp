6132a772f4a94554e53cef63fc9a8d43494b0b9b
Milos Vasic <milos85vasic@gmail.com>
2021-04-15 15:25:28 +0200

    Print out the Boost library version.

--- Native/library.cpp
+++ Native/library.cpp
@@ -1,8 +1,14 @@
 #include "library.h"
 
+#include <boost/version.hpp>
+
 #include <iostream>
 
 void hello() {
 
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Using Boost "
+              << BOOST_VERSION / 100000 << "."
+              << BOOST_VERSION / 100 % 1000 << "."
+              << BOOST_VERSION % 100
+              << std::endl;
 }
