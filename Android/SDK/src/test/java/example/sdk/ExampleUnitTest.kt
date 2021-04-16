d49c258e281efd1b1fce09595f22bb6f66cfb8d0
Milos Vasic <milos85vasic@gmail.com>
2021-04-16 14:30:12 +0200

    Empty Android project.

---
+++ Android/SDK/src/test/java/example/sdk/ExampleUnitTest.kt
@@ -0,0 +1,14 @@
+package example.sdk
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+
+        assertEquals(4, 2 + 2)
+    }
+}
