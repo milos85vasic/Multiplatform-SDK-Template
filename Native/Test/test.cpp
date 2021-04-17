266d11979285bc4056a57ea20d23f587ab0c67e2
Milos Vasic <milos85vasic@gmail.com>
2021-04-17 13:18:12 +0200

    Android performs (Cmake) build properly.

--- Native/Test/test.cpp
+++ Native/Test/test.cpp
@@ -1,9 +1,12 @@
-#include <string>
 #include "library.h"
+
+#include <string>
+#include <iostream>
 #include <gtest/gtest.h>
 
 TEST(BoostLibraryTest, AvailabilityTest) {
 
     auto version = getBoostVersion();
+    std::cout << "The Boost library version: " << version << std::endl;
     ASSERT_FALSE(version.empty());
 }
